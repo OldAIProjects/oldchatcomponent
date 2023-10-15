1531a6c9d2a85d2f7d0b82a8ede969561c6ca1a2
Josh XT <josh@devxt.com>
2023-10-15 13:44:39 -0400

    Updates

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+};
